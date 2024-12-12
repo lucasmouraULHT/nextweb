356cfc18524811a63beb7e2abb5084ff393babc9
Lucas Moura - a22305109 <160148657+lucasmouraULHT@users.noreply.github.com>
2024-12-12 15:54:58 +0000

    nao sei

--- app/Produto/page.tsx
+++ app/Produto/page.tsx
@@ -4,5 +4,5 @@ import React from 'react';
 import { Product } from '@/app/models/interfaces';
 
 export default function Produto({ id, title, price, description }: Product) {
-    return <div>{id} {title},{price},{description} </div>;
+    return <div>{id} {title} {price} {description} </div>;
 }
--- app/Tecnologia/page.tsx
+++ app/Tecnologia/page.tsx
@@ -6,7 +6,7 @@ import { TecnologiaC } from '@/app/models/interfacesT';
 export default function Tecnologia({ title, description, rating }: TecnologiaC) {
     return (
         <div>
-            {title},{description} ,{rating} 
+            {title} {description} {rating} 
         </div>
     );
 }
