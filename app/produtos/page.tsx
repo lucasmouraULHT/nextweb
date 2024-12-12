f6d248c37dea35a4e0b1ce7bda97173881cbc208
Lucas Moura - a22305109 <160148657+lucasmouraULHT@users.noreply.github.com>
2024-12-12 15:48:26 +0000

    vou chumbar a web porque nao tenho um link

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -2,16 +2,14 @@
 
 import useSWR from 'swr';
 import Card from '@/components/Card/Card';
-import { Product } from '../models/interfaces';
+import { Product } from '@/app/models/interfaces';
+
 
 
 
 export default function ProductsPage() {
     const fetcher = (url: string) => fetch(url).then((res) => res.json());
-    const { data, error, isLoading } = useSWR<Product[]>(
-        '/api/products', 
-        fetcher
-    );
+    const { data, error, isLoading } = useSWR<Product[]>('https://deisishop.pythonanywhere.com/products', fetcher);
     
 
     if (error) return <div>Failed to load</div>;
