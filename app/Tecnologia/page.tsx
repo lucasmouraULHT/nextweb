148ad0686d5e44d87c2f9a83820624a25cca2a7f
Lucas Moura - a22305109 <160148657+lucasmouraULHT@users.noreply.github.com>
2024-12-12 11:40:12 +0000

    tirei default

--- app/Tecnologia/page.tsx
+++ app/Tecnologia/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { TecnologiaC } from '@/app/models/interfacesT';
 
-export default function Tecnologia({ title, description, rating }: TecnologiaC) {
+export function Tecnologia({ title, description, rating }: TecnologiaC) {
     return (
         <div>
             {title},{description} ,{rating} 
