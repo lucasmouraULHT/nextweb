e671f23636c667f2dadfdc0da616b4fb4d4244a5
Lucas Moura - a22305109 <160148657+lucasmouraULHT@users.noreply.github.com>
2024-12-21 12:31:53 +0000

    aaaaa

--- app/Produto/page.tsx
+++ app/Produto/page.tsx
@@ -3,6 +3,19 @@
 import React from 'react';
 import { Product } from '@/app/models/interfaces';
 
-export default function Produto({ id, title, price, description }: Product) {
-    return <div>{id} {title} {price} {description} </div>;
+interface ProdutoProps {
+  product: Product;
+}
+
+export default function Produto({ product }: ProdutoProps) {
+  const { id, title, price, description, image } = product;
+  return (
+    <div>
+      <h2>{title}</h2>
+      <img src={image} alt={title} style={{ width: '150px', height: '150px' }} />
+      <p>ID: {id}</p>
+      <p>Preço: {price}</p>
+      <p>Descrição: {description}</p>
+    </div>
+  );
 }
