032faf46e32691d36c1d092569799d65725d1a65
Lucas Moura - a22305109 <160148657+lucasmouraULHT@users.noreply.github.com>
2024-12-29 13:04:29 +0000

    lab12 aaaaaaaaaa

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -4,13 +4,15 @@ import useSWR from 'swr';
 import Card from '@/components/Card/Card';
 import { Product } from '@/app/models/interfaces';
 
-
-
-
 export default function ProductsPage() {
     const fetcher = (url: string) => fetch(url).then((res) => res.json());
     const { data, error, isLoading } = useSWR<Product[]>('https://deisishop.pythonanywhere.com/products', fetcher);
-    
+
+    const addItemToCart = (product: Product) => {
+        const cart = JSON.parse(localStorage.getItem('cart') || '[]');
+        cart.push(product);
+        localStorage.setItem('cart', JSON.stringify(cart));
+    };
 
     if (error) return <div>Failed to load</div>;
     if (isLoading) return <div>Loading...</div>;
@@ -19,9 +21,8 @@ export default function ProductsPage() {
     return (
         <>
             {data.map((product) => (
-                <Card key={product.id} id={product.id} title={product.title} price={product.price} description={product.description} image={product.image} />
+                <Card key={product.id} product={product} addItemToCart={addItemToCart} />
             ))}
-            
         </>
     );
 }
